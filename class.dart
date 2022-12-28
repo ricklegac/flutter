c292341e4a456cb5fd126d2e7a7a67daab90a6d6
ricklegac <rick.legac@gmail.com>
2022-12-27 23:15:22 -0300

    clases

--- class.dart
+++ class.dart
@@ -1,8 +1,15 @@
 void main(){
     final wolwerine = new Heroe();
+    wolwerine.nombre = 'Logan';
+    wolwerine.poder = 'poder_xd'; 
     print (wolwerine);
 
 }
 class Heroe{
-    
+    String? nombre;
+    String? poder;
+    @override
+    String toString(){
+        return 'heroe nombre: ${this.nombre} poder: ${this.poder}';
+    }
 }
