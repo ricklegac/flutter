c9cf4d9e875dbc23924a3a5ac6a49196ec314148
ricklegac <rick.legac@gmail.com>
2023-01-25 20:50:38 -0300

    ta

--- counter_app/lib/screens/home_screen.dart
+++ counter_app/lib/screens/home_screen.dart
@@ -7,9 +7,22 @@ class  HomeScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return const Center(
-      child: Text('HOME SCREEN'),
-      );
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 113, 154, 226)  ,
+      appBar: AppBar(
+        title: const Text('HomeScreen'),
+        elevation: 2.0  , 
+      ), 
+      body:  Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const <Widget>[
+          Text('numero de clicks'),
+          Text('aaa'),
+        ],
+           
+      ),
+    );
   }
 
 }
