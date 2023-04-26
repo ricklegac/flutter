d65a34715106a8bd27473f355ff68835ba8e024e
ricklegac <rick.legac@gmail.com>
2023-04-25 23:07:56 -0400

    routes

--- widgets_app/lib/router/apps_routes.dart
+++ widgets_app/lib/router/apps_routes.dart
@@ -1,6 +1,14 @@
+import 'package:widgets_app/models/models.dart';
 import 'package:widgets_app/screens/screens.dart';
-class AppRoutes{
-  static const initialRoute = 'home'; //estatico como global
+class AppRoutes{ // si no tiene un _ es porque se esta exportando, es publico _AppRoutes  
+static const initialRoute = 'home'; //estatico como global
+static final menuOption = <MenuOption>[
+  MenuOption(route: 'home', name: 'Home Screen', screen: const HomeScreen() , icon: Icons.home),
+  MenuOption(route: 'listview1', name: 'List View 1', screen: const Listview1Screen() , icon: Icons.list_sharp),
+  MenuOption(route: 'listview2', name: 'List View 2', screen: const Listview2Screen() , icon: Icons.list_alt_outlined),
+  MenuOption(route: 'alert', name: 'alert', screen: const AlertScreen() , icon: Icons.add_alert_sharp),
+  MenuOption(route: 'card', name: 'card', screen: const CardScreen() , icon: Icons.card_giftcard),
+];
   static Map<String, Widget Function(BuildContext)> routes = {
     'home'      :(BuildContext context )=> const HomeScreen(),
     'listview1' :(BuildContext context) => const Listview1Screen(),
