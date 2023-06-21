583f62f2c1d2907af5f80a1fde930d6073d1d2dc
ricklegac <rick.legac@gmail.com>
2023-06-21 15:48:23 -0400

    custom type 2

--- widgets_app/lib/widgets/custom_card_type_2.dart
+++ widgets_app/lib/widgets/custom_card_type_2.dart
@@ -7,15 +7,24 @@ class CustomCardType2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return Card(
+      elevation: 10,
+      child: Column(
+        children:  [
+           const FadeInImage(
+            placeholder: AssetImage('assets/jar-loading.gif'), 
+            image:  NetworkImage('https://hips.hearstapps.com/hmg-prod/images/ballooning-pink-sky-rcg2wh-1531753930.jpg?crop=1xw:1xh;center,top&resize=980:*'),
+            width: double.infinity,       
+            height: 230,  
+            fit: BoxFit.cover,   
+            ),
+            Container(
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.only(right: 20, top: 10),
+              child: const Text('texto cualquiera'),
+
+            ),
+        ],
       ),
     );
   }
