04369e7772ab4614881a7dd786a05971cbed4934
ricklegac <rick.legac@gmail.com>
2023-06-23 11:12:28 -0400

    dialog

--- widgets_app/lib/screens/alert_screen.dart
+++ widgets_app/lib/screens/alert_screen.dart
@@ -5,7 +5,14 @@ import 'package:flutter/material.dart';
 class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
-  
+  void DisplayDialog (BuildContext context){
+    showDialog(
+      context: context, 
+      builder: (context){
+        return const Text('aaaaa');
+      }
+      );
+  }
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
