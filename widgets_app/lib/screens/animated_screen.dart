0b08cf045f9d411f51de8309f854ec52b492af40
ricklegac <rick.legac@gmail.com>
2023-06-23 16:12:16 -0400

    colors change

--- widgets_app/lib/screens/animated_screen.dart
+++ widgets_app/lib/screens/animated_screen.dart
@@ -1,15 +1,55 @@
-import 'package:flutter/material.dart';
+import 'dart:math';
 
-class AnimatedScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+class AnimatedScreen extends StatefulWidget {
    
   const AnimatedScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
+
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.red;
+  BorderRadiusGeometry _BorderRadius = BorderRadius.circular(20);
+  //int _i = 100;
+  void changeShape(){
+  var generatedColor = Random().nextInt(Colors.primaries.length);
+  Colors.primaries[generatedColor];
+  _width += 10;
+  _height += 10;
+  _color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+  setState(() {
+    
+  });
+}
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('AnimatedScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('animated widget'),
       ),
+      body:  Center(
+         child: Container(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _BorderRadius,
+          ),
+
+         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child:  const Icon(Icons.play_arrow, size: 30),
+        onPressed:( ){
+          changeShape();
+        }
+         ),
     );
   }
 }
