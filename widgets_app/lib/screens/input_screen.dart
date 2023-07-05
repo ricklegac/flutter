0beee39b4f87f22428e15d9326482b4560b2c517
ricklegac <rick.legac@gmail.com>
2023-07-05 16:22:33 -0400

    input

--- widgets_app/lib/screens/input_screen.dart
+++ widgets_app/lib/screens/input_screen.dart
@@ -5,6 +5,7 @@ class InputScreen extends StatelessWidget {
   const InputScreen({Key? key}) : super(key: key);
   
   @override
+  
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
@@ -18,7 +19,24 @@ class InputScreen extends StatelessWidget {
               TextFormField(
                 autofocus: true,
                 autocorrect: true,
-                initialValue: '',
+                initialValue: 'valor inicial',
+                onChanged: (value) => print('$value'),
+                validator: (value) {
+                  if (value ==null) return 'campo requerido';
+                  return value.length<3 ? 'Minimo 3 letras' : null;
+                },
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                decoration: const InputDecoration(
+                  hintText: 'Nombre de usuario',
+                  labelText: 'Nombre',
+                  helperText: 'solo letras',
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                autofocus: true,
+                autocorrect: true,
+                initialValue: 'valor inicial',
                 onChanged: (value) => print('$value'),
                 validator: (value) {
                   if (value ==null) return 'campo requerido';
