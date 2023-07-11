31c7f2143670e3752d255c57f364a032dcbf7ead
ricklegac <rick.legac@gmail.com>
2023-07-11 15:45:11 -0400

    envolvemos form

--- widgets_app/lib/screens/input_screen.dart
+++ widgets_app/lib/screens/input_screen.dart
@@ -27,20 +27,31 @@ class InputScreen extends StatelessWidget {
       body: SingleChildScrollView(
          child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            children: const [
-              CustomInputWidget(hinText: 'Nombre usuario', icon: Icons.line_style, helperText: 'nombre', textinput: null,),
-              SizedBox(height: 16.0),
-              
-              CustomInputWidget(hinText: 'Apellido usuario', icon: Icons.line_style, helperText: 'apellido',),
-              SizedBox(height: 16),
-
-              CustomInputWidget(hinText: 'Correo usuario', icon: Icons.line_style, helperText: 'correo', textinput: TextInputType.emailAddress),
-              SizedBox(height: 16),
-
-              CustomInputWidget(hinText: 'password', icon: Icons.password, helperText: 'password', obscureText: true,),
-
-            ],
+          child: Form(
+            child: Column( //envolvemos este en un widget form porque es lo que contiene los datos
+              children:   [
+                const CustomInputWidget(hinText: 'Nombre usuario', icon: Icons.line_style, helperText: 'nombre', textinput: null,),
+                const SizedBox(height: 16.0),
+                
+                const CustomInputWidget(hinText: 'Apellido usuario', icon: Icons.line_style, helperText: 'apellido',),
+                const SizedBox(height: 16),
+          
+                const CustomInputWidget(hinText: 'Correo usuario', icon: Icons.line_style, helperText: 'correo', textinput: TextInputType.emailAddress),
+                const SizedBox(height: 16),
+          
+                const CustomInputWidget(hinText: 'password', icon: Icons.password, helperText: 'password', obscureText: true,),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center(child: Text('guardar')),
+                  ),
+                  onPressed: () {
+          
+                  } , 
+                  )
+              ],
+            ),
           ),
          ),
       ),
