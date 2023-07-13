96a84187368feff7b68be33f43a7e9507b4be6e9
ricklegac <rick.legac@gmail.com>
2023-07-13 10:52:37 -0400

    mediaquery, listview

--- widgets_app/lib/screens/listview_builder_screen.dart
+++ widgets_app/lib/screens/listview_builder_screen.dart
@@ -7,14 +7,23 @@
       @override
       Widget build(BuildContext context) {
         return  Scaffold(
-          body: ListView.builder(
-            itemCount: 10,
-            itemBuilder: (BuildContext context, int index){
-              return  FadeInImage(
-              placeholder: const AssetImage('assets/jar-loading.gif'), 
-              image: NetworkImage('https://picsum.photos/seed/picsum/200/300?image=$(index+1)'),
-              );
-            }
+          body: MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            removeBottom: true,
+
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index){
+                return  FadeInImage(
+                fit: BoxFit.cover,
+                height: 300,
+                width: double.infinity,
+                placeholder: const AssetImage('assets/jar-loading.gif'), 
+                image: NetworkImage('https://picsum.photos/id/237/500/300?image=${index +1}'),
+                );
+              },
+            ),
           ),
           // appBar:  AppBar(
           //   title:  const Text('List View Builder'),
