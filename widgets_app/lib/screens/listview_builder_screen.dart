327dbb352363d0ec3acbe8506e4c9875ddd4cf9f
ricklegac <rick.legac@gmail.com>
2023-07-13 10:38:48 -0400

    listviewbuilder

--- widgets_app/lib/screens/listview_builder_screen.dart
+++ widgets_app/lib/screens/listview_builder_screen.dart
@@ -7,10 +7,19 @@
       @override
       Widget build(BuildContext context) {
         return  Scaffold(
-          appBar:  AppBar(
-            title:  const Text('List View Builder'),
+          body: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index){
+              return  FadeInImage(
+              placeholder: const AssetImage('assets/jar-loading.gif'), 
+              image: NetworkImage('https://picsum.photos/seed/picsum/200/300?image=$(index+1)'),
+              );
+            }
           ),
-           
+          // appBar:  AppBar(
+          //   title:  const Text('List View Builder'),
+          // ),
+          
         );
       }
     }
