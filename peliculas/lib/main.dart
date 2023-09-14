c49f06fbeec76d1bf7c6a2ed34a2fe253e560263
ricklegac <rick.legac@gmail.com>
2023-09-14 15:03:43 -0400

    await error

--- peliculas/lib/main.dart
+++ peliculas/lib/main.dart
@@ -4,7 +4,7 @@ import 'package:peliculas/screens/screens.dart';
 import 'package:provider/provider.dart';
 
 
-void main() => runApp(const MyApp());
+void main() => runApp( AppState());
 class AppState extends StatelessWidget {
 
 
@@ -12,8 +12,9 @@ class AppState extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => MovieProvider()),
+        ChangeNotifierProvider(create: (_) => MovieProvider(), lazy: false,),
       ],
+      // lazy es cuando el provider inicialice apenas se inice
       child: MyApp(),
     );
       
--- peliculas/lib/providers/movies_provider.dart
+++ peliculas/lib/providers/movies_provider.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 class MovieProvider extends ChangeNotifier{ // para que sea un provider debe de extender de changenotifier
-
+  String _apiKey = 'dce3b23572e77f35d099620c9feae51d';
+  String _baseUrl = 'https://api.themoviedb.org';
+  String _language = 'es-ES';
   MovieProvider(){
     print('movie provider inicializado');
     this.getNowPlaying();
     }
-  getNowPlaying()
-  {
-    print('getnowplaying');
-  } 
+    Future<void> getNowPlaying() async {
+    var url = Uri.https(this._baseUrl, '3/movie/now_playing',
+        {'api_key': _apiKey, 'language': _language, 'page': '1'});
+
+    try {
+      final response = await http.get(url);
+      print(response.body);
+      if (response.statusCode == 200) {
+        // Successful response, process the data here
+        final data = response.body;
+        print(data);
+      } else {
+        // Handle HTTP error status codes
+        print('HTTP Error: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle exceptions that may occur during the request
+      print('Error: $e');
+    }
+  }
 }
+
+
+
