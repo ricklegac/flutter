8a651e29cc44ebb23ef9f0ed7db7cff0cee1ffad
ricklegac <rick.legac@gmail.com>
2023-10-10 16:34:43 -0300

    small changes

--- peliculas/lib/screens/home_screen.dart
+++ peliculas/lib/screens/home_screen.dart
@@ -29,7 +29,6 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             CardSwipper(movies: movieProvider.onDisplayMovies),
-            
             MovieSlider (movies: movieProvider.popularMovies, titulo: 'Prueba Popular', 
               onNextPage: () => movieProvider.getPopularMovies()
         )
