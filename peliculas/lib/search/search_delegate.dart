4ba1d0d9eb0b86932fd4269c6da03ed52fbf7dcc
ricklegac <rick.legac@gmail.com>
2023-10-15 23:00:19 -0300

    delegate

--- peliculas/lib/search/search_delegate.dart
+++ peliculas/lib/search/search_delegate.dart
@@ -82,12 +82,16 @@ class _MovieItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    movie.heroId = 'search -${movie.id}';
     return ListTile(
-      leading: FadeInImage(
-        placeholder: const AssetImage('assets/no-image.jpg'),
-        image: NetworkImage(movie.fullPosterImg),
-        width: 200,
-        fit: BoxFit.contain,
+      leading: Hero(
+        tag: movie.heroId!,
+        child: FadeInImage(
+          placeholder: const AssetImage('assets/no-image.jpg'),
+          image: NetworkImage(movie.fullPosterImg),
+          width: 200,
+          fit: BoxFit.contain,
+        ),
       ),
     title: Text(movie.title),
     // ignore: avoid_print
