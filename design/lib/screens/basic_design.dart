d7c59554a8dd49b036d0dfc3a67a72c4f9d7185d
ricklegac <rick.legac@gmail.com>
2023-10-16 16:45:22 -0300

    icons

--- design/lib/screens/basic_design.dart
+++ design/lib/screens/basic_design.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class BasicScreen extends StatelessWidget {
@@ -5,9 +7,77 @@ class BasicScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg')),
+    return  const Scaffold(
+      body: Column(
+        children: [ 
+          Image(image: AssetImage('lib/assets/landscape.webp')),
+          Titulos(),
+          Row(
+            children: [
+              Botones(icon: Icons.call, texto: 'call'),
+              Botones(icon: Icons.route, texto: 'route'),
+              Botones(icon: Icons.share, texto: 'share'),
+            ]
+          ),
+
+        ]
+      ),
+    );
+  }
+}
+
+class Botones extends StatelessWidget {
+  final IconData icon;
+  final String texto;
+  const Botones({
+    super.key,
+    required this.icon,
+    required this.texto
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Icon(icon),
+        Text('$texto'),
+        SizedBox(width: 100,)
+      ],
+
+      
+      
+
+    );
+  }
+}
+class Titulos extends StatelessWidget {
+  const Titulos({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+      child:  Row(
+        //mainAxisAlignment: MainAxisAlignment.spaceAround , // de un row es horizontal de un column es vertical 
+        children: [
+          const Column (
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Titulo cualquiera .. aaa bbb ..cc', style: TextStyle(fontWeight: FontWeight.bold),),
+              Text('Subitulo cualquiera'),
+          ],
+            
+          ),
+          Expanded(
+            child: Container()
+          ),
+          const Icon(Icons.star, color: Colors.red),
+          const Text('41'),
+    
+        ],
+    
       ),
     );
   }
