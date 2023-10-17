6e3550b80278d56637f0f4d421bb8679ad32a953
ricklegac <rick.legac@gmail.com>
2023-10-17 10:43:50 -0300

    terminado

--- design/lib/screens/basic_design.dart
+++ design/lib/screens/basic_design.dart
@@ -7,20 +7,40 @@ class BasicScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return  const  Scaffold(
       body: Column(
+        
         children: [ 
-          Image(image: AssetImage('lib/assets/landscape.webp')),
-          Titulos(),
-          Row(
+           Image(image: AssetImage('lib/assets/landscape.webp')),
+          
+           Titulos(),
+           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Botones(icon: Icons.call, texto: 'call'),
-              Botones(icon: Icons.route, texto: 'route'),
-              Botones(icon: Icons.share, texto: 'share'),
+              Botones(icon: Icons.call, texto: 'call', colores: Colors.red,),
+              Botones(icon: Icons.route, texto: 'route',colores: Colors.green),
+              Botones(icon: Icons.share, texto: 'share', colores: Colors.blue),
             ]
           ),
+          Texto(),
+          
 
-        ]
+        ],
+      ),
+    );
+  }
+}
+
+class Texto extends StatelessWidget {
+  const Texto({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: const Text('Consectetur ipsum do consequat adipisicing cupidatat aliqua ut labore aliquip proident et non duis. Quis veniam fugiat laboris anim laborum cillum. Id ad veniam ut in incididunt ea magna nulla et adipisicing officia nulla adipisicing. Enim est nisi anim deserunt deserunt exercitation aute incididunt aute nisi aliquip. Amet adipisicing non elit mollit magna sit sunt proident.'
       ),
     );
   }
@@ -29,8 +49,10 @@ class BasicScreen extends StatelessWidget {
 class Botones extends StatelessWidget {
   final IconData icon;
   final String texto;
+  final MaterialColor colores;
   const Botones({
     super.key,
+    required this.colores,
     required this.icon,
     required this.texto
   });
@@ -38,10 +60,12 @@ class Botones extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        Icon(icon),
-        Text('$texto'),
-        SizedBox(width: 100,)
+        Icon(icon, color: colores, size: 30),
+        Text(texto),
+
+        
       ],
 
       
@@ -73,7 +97,7 @@ class Titulos extends StatelessWidget {
           Expanded(
             child: Container()
           ),
-          const Icon(Icons.star, color: Colors.red),
+          const Icon(Icons.star, color: Colors.red, size: 30,),
           const Text('41'),
     
         ],
