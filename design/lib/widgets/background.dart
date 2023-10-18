2f8e48a624ec7ade8ed300d44599adb06f0a7dda
ricklegac <rick.legac@gmail.com>
2023-10-18 15:14:45 -0300

    cuadrado efecto background

--- design/lib/widgets/background.dart
+++ design/lib/widgets/background.dart
@@ -1,13 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Background extends StatelessWidget {
    
   const Background({Key? key}) : super(key: key);
-  
-  @override
-  Widget build(BuildContext context) {
-    return  Container(
-      decoration: const BoxDecoration(
+  final boxDecoration =  const BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
@@ -16,7 +14,47 @@ class Background extends StatelessWidget {
             Color(0xff202333),
           ]
         ),
+      );
+  @override
+  Widget build(BuildContext context) {
+    return  Stack(
+      children:[
+        Container(
+        decoration: boxDecoration,
       ),
+      const Positioned(
+        top: -100,
+        left: -30,
+        child:  _PinkBox()
+      ),
+
+      ] 
+    );
+  }
+}
+
+class _PinkBox extends StatelessWidget {
+  const _PinkBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: -pi / 5,
+      child: Container(
+        child: Container(
+          width: 360,
+          height: 360,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            gradient: const LinearGradient(
+              colors: [
+              Color.fromRGBO(236, 98, 188, 1),
+              Color.fromRGBO(241, 142, 172, 1),
+              ])
+           ),
+        ),
+      ),
+
     );
   }
 }
