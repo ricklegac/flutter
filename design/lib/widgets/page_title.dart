6e2011836e7336e90e72e5eccbbf69a810a96945
ricklegac <rick.legac@gmail.com>
2023-10-19 15:56:06 -0300

    page title termiado

--- design/lib/widgets/page_title.dart
+++ design/lib/widgets/page_title.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 
 
 class PageTile extends StatelessWidget {
+  
   const PageTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container(
-      child: const Column(
+    const textStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white, decoration: TextDecoration.none);
+    return  const SafeArea(
+      child:  Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Classify Transaction'),
+          Text('Classify Transaction', style: textStyle,),
+          SizedBox(height: 10,),
+          Text('Classify this transaction into a particular category', style: TextStyle(fontSize: 12, color: Colors.white,decoration: TextDecoration.none),),
         ],
       ),
     );
