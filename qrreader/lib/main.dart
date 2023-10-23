4d10da9ce9dff3fd5e4ec1bec33782ec45f1d49a
ricklegac <rick.legac@gmail.com>
2023-10-23 09:32:43 -0300

    initial routes

--- qrreader/lib/main.dart
+++ qrreader/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qrreader/pages/pages.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,15 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      title: 'QR Reader',
+      initialRoute: 'home',
+      routes: {
+        'home':( _ ) => const HomePage(),
+        'mapas': ( _ ) => const MapaPage(),
+      }
+      
+     
     );
   }
 }
---
+++ qrreader/lib/pages/pages.dart
@@ -0,0 +1,2 @@
+export 'package:qrreader/pages/mapa_page.dart';
+export 'package:qrreader/pages/home_page.dart';
