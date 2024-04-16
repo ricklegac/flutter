95438946f2894ab1d5b1ff5742bfce98a7dd1b53
ricklegac <rick.legac@gmail.com>
2024-04-16 09:40:21 -0400

    productos app

---
+++ productos_app/lib/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
+
+class LoginScreen extends StatelessWidget {
+   
+  const LoginScreen({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: AuthBackground(),
+    );
+  }
+}
---
+++ productos_app/lib/widgets/auth_backgrund.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthBackground extends StatelessWidget {
+  const AuthBackground({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      width: double.infinity,
+      height: double.infinity,
+    );
+  }
+}
