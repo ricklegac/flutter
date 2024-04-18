462b4f4cc876d4f9e0598a78cbe01600d003f678
ricklegac <rick.legac@gmail.com>
2024-04-18 14:04:50 -0400

    login email

--- productos_app/lib/screens/login_screen.dart
+++ productos_app/lib/screens/login_screen.dart
@@ -9,15 +9,61 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       body: AuthBackground(
+        // ignore: avoid_unnecessary_containers
         child: Container(
           child: SingleChildScrollView(
             child: Column(children: [
               const SizedBox(height: 300,),
-              CardContainer(),
+              CardContainer(
+                child:  Column(children: [
+                  const SizedBox(height: 10,), 
+                  Text('Login', style: Theme.of(context).textTheme.headline4),
+                  const SizedBox(height: 10,),
+                  _Formulario(),   
+                ],)
+
+              ),
+              const SizedBox(height: 20,),
+              const Text('prueba prueba preba ', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))
             ],),
           )
         )
       ),
+    );
+  }
+}
+class _Formulario extends StatelessWidget {
+  const _Formulario({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        child: Column(children: [
+          TextFormField(
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.deepOrangeAccent)
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.deepOrangeAccent,
+                  width: 2,
+                ), 
+              ),
+              hintText: 'ejemplo@ejemplo.com',
+              labelText: 'Email',
+              labelStyle: TextStyle(
+                color: Colors.grey,
+              ),
+              prefixIcon: Icon(Icons.alternate_email, color: Colors.deepOrangeAccent)
+            ),
+            
+          )
+        ],)
+      )
     );
   }
 }
--- productos_app/lib/widgets/card_container.dart
+++ productos_app/lib/widgets/card_container.dart
@@ -2,21 +2,28 @@ import 'package:flutter/material.dart';
 
 class CardContainer extends StatelessWidget {
   
+  final Widget child;
 
+   CardContainer({
+    super.key,
+    required this.child,
+  });
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 30,),
       
       child: Container(
+        padding: const EdgeInsets.all(20),
         width: double.infinity,
-        height: 300, //TODO: borrar 
+        //height: 300, //TODO: borrar 
         decoration: createCardShape(),
+        child: child, //el child que llamaamos en CardContainer
       ),
     );
   }
 
-  BoxDecoration createCardShape() {
+  BoxDecoration createCardShape() { // la sombra del card
     return BoxDecoration(
       color: Colors.white,
       borderRadius: BorderRadius.circular(30),
