a823d6f6524e6af64926bfe3809e1158d52390e3
ricklegac <rick.legac@gmail.com>
2024-04-30 15:19:02 -0400

    product image

--- productos_app/lib/widgets/product_image.dart
+++ productos_app/lib/widgets/product_image.dart
@@ -5,7 +5,32 @@ class ProductImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: 10, left: 10, right: 10),
-      child: Container());
+      padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+      child: Container(
+        width: double.infinity,
+        height: 450,
+        decoration: _BuildBoxDecoration(),
+        child: const ClipRRect(
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+          child: FadeInImage(
+            image: AssetImage('./assets/jar-loading.gif'),
+            placeholder: AssetImage('./assets/jar-loading.gif'),
+            fit: BoxFit.cover,
+          ),
+        ),
+
+      ));
   }
+
+  BoxDecoration _BuildBoxDecoration() => const  BoxDecoration(
+    color: Colors.red,
+    borderRadius: BorderRadius.only(topLeft: Radius.circular(45), topRight: Radius.circular(45)),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black,
+        blurRadius: 10,
+        offset: Offset(0, 5)
+      )
+    ]
+  );
 }
